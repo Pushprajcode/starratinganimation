84f7c2ff325479197c315ace13e83f1ba265cf22
Pushprajcode <pushpraj.singh@appinventiv.com>
2023-01-11 07:33:44 +0530

    starrating

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import StarRating from './src/starrating';
+import AnimationScreen from './src/screens/animation';
+import Interpolation from './src/screens/animation/interpolation';
+import FlatlistAnimation from './src/screens/animation/flatlistanimation';
+import Ballmove from './src/screens/animation/ballmove';
+import ExpensesScreen from './src/screens/payment';
+import SummeryScreen from './src/screens/payment/test';
+import BugetLimit from './src/screens/bugetlimit';
+
+export default function App() {
+  return (
+    <>
+      {/* <StarRating /> */}
+      {/* <AnimationScreen />
+      <Interpolation /> */}
+      {/* <FlatlistAnimation /> */}
+      {/* <Ballmove /> */}
+      {/* <ExpensesScreen /> */}
+      {/* <Test /> */}
+      {/* <SummeryScreen /> */}
+      {/* <BugetLimit /> */}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
